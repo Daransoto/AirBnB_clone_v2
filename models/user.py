f52025e941f9e7bffff215116f212b433656c286
Daransoto <901@holbertonschool.com>
2019-12-19 15:48:44 +0000

    Removed conditional

--- models/user.py
+++ models/user.py
@@ -18,18 +18,9 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = 'users'
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade='delete')
-        reviews = relationship("Review", backref="user", cascade='delete')
-
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade='delete')
+    reviews = relationship("Review", backref="user", cascade='delete')
