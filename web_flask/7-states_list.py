af4432f3964d7465bfc0fe18a67d2a12bee70419
Daransoto <901@holbertonschool.com>
2020-01-21 20:28:10 +0000

    Removed unnecesary routes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,42 +6,6 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_and_var(text):
-    return 'C ' + text.replace('_', ' ')
-
-
-@app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
-@app.route('/python/<text>', strict_slashes=False)
-def Python_and_var(text):
-    return 'Python ' + text.replace('_', ' ')
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return str(n) + ' is a number'
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_even(n):
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     return render_template('7-states_list.html', states=storage.all("State"))
